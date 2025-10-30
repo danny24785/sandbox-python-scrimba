b0b3c42a539db8f1d0767aa2324004f5e6598c78
dhalbesma <d.halbesma@blisdigital.com>
2025-10-30 21:41:23 +0100

    more conditionals

---
+++ conditionals.py
@@ -0,0 +1,11 @@
+is_raining = False
+is_cold = False
+print("Good morning")
+if is_raining and is_cold:
+    print("Bring an umbrella, and jacket")
+elif is_raining and not(is_cold):
+    print("Bring an umbrella")
+elif not(is_raining) and is_cold:
+    print("Bring a jacket")
+else:
+    print("shirt is fine")
